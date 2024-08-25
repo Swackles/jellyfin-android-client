d0c54ac673a12952f5138e00b501e1b706336763
Swackles <swackles@users.noreply.github.com>
2024-08-26 01:42:58 +0300

    feat: Test host with different protocols

--- domain/auth/src/main/java/com/swackles/jellyfin/domain/auth/AuthenticatorUseCase.kt
+++ domain/auth/src/main/java/com/swackles/jellyfin/domain/auth/AuthenticatorUseCase.kt
@@ -1,5 +1,6 @@
 package com.swackles.jellyfin.domain.auth
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.swackles.jellyfin.data.jellyfin.enums.JellyfinResponses
@@ -15,6 +16,7 @@ import com.swackles.jellyfin.domain.auth.models.AuthCredentials
 import com.swackles.jellyfin.domain.auth.models.AuthenticatorResponse
 import io.ktor.http.URLParserException
 import org.jellyfin.sdk.api.client.exception.SecureConnectionException
+import org.jellyfin.sdk.api.client.exception.TimeoutException
 import java.time.LocalDateTime
 import javax.inject.Inject
 
@@ -25,6 +27,10 @@ open class AuthenticatorUseCase @Inject constructor(
     ) {
     open val authenticatedUser: LiveData<User?> = AuthenticatorUseCase.authenticatedUser
 
+    /**
+     * Logs in the last used user
+     * @param serverId The id of the server to log in to. When null, the server will be the last used server
+     */
     suspend fun loginLastUsedUser(serverId: Long? = null): AuthenticatorResponse {
         val serverUser = userRepository.getLastActiveUserAndServer(serverId)
             ?: return AuthenticatorResponse.NO_USER
@@ -32,30 +38,53 @@ open class AuthenticatorUseCase @Inject constructor(
         return login(serverUser)
     }
 
+    /**
+     * Logs in a user with existing credentials, used for already authenticated users
+     */
     suspend fun login(user: UserAndServer): AuthenticatorResponse {
         return try {
             handleResponse(repository.login(user.server.host, user.externalId, user.token, user.deviceId))
-        } catch (ex: URLParserException) {
-            AuthenticatorResponse.INVALID_URL
-        } catch (ex: SecureConnectionException) {
-            AuthenticatorResponse.INVALID_URL
+        } catch (ex: Exception) {
+            return when(ex) {
+                is URLParserException, is SecureConnectionException, is TimeoutException -> AuthenticatorResponse.INVALID_URL
+                else -> throw ex
+            }
         }
     }
 
+    /**
+     * Logs in a user with the provided credentials, used for new users
+     */
     suspend fun login(credentials: AuthCredentials): AuthenticatorResponse {
-        val host = credentials.host
+        // use if host in AuthCredentials is null use the serverId of the current user
+        val hosts = generateAcceptableHosts(credentials.host
             ?: serverRepository.getServer(authenticatedUser.value!!.serverId)?.host
-            ?: return AuthenticatorResponse.UNKNOWN_ERROR
+            ?: return AuthenticatorResponse.UNKNOWN_ERROR)
 
-        return try {
-            handleResponse(repository.login(host, credentials.username, credentials.password))
-        } catch (ex: URLParserException) {
-            AuthenticatorResponse.INVALID_URL
-        } catch (ex: SecureConnectionException) {
-            AuthenticatorResponse.INVALID_URL
+        for (host in hosts) {
+            return try {
+                Log.d("AuthenticatorUseCase", "Attempting to login to $host")
+                handleResponse(repository.login(host, credentials.username, credentials.password))
+            } catch (ex: Exception) {
+                when(ex) {
+                    is URLParserException, is SecureConnectionException, is TimeoutException -> {
+                        if (host == hosts.last()) AuthenticatorResponse.INVALID_URL
+                        else continue
+                    }
+
+                    else -> throw ex
+                }
+            }
         }
+
+        // Code should not reach here, so return unknown error
+        return AuthenticatorResponse.UNKNOWN_ERROR
     }
 
+    /**
+     * Logs in a user with the provided id
+     * @param id The id of the user to log in
+     */
     suspend fun login(id: Long): AuthenticatorResponse {
         return login(userRepository.getUserAndServer(id) ?: return AuthenticatorResponse.NO_USER)
     }
@@ -119,7 +148,28 @@ open class AuthenticatorUseCase @Inject constructor(
         return AuthenticatorResponse.SUCCESS
     }
 
+    private fun generateAcceptableHosts(host: String): List<String> {
+        val hosts = mutableListOf<String>()
+
+        // Even if it supports HTTP, we will always try HTTPS first
+        if (host.startsWith(URL_RROTOCOL_HTTP)) {
+            hosts.add(host.replace(URL_RROTOCOL_HTTP, URL_RROTOCOL_HTTPS))
+            hosts.add(host)
+        } else if (host.startsWith(URL_RROTOCOL_HTTPS)) {
+            hosts.add(host)
+            hosts.add(host.replace(URL_RROTOCOL_HTTPS, URL_RROTOCOL_HTTP))
+        } else {
+            hosts.add(URL_RROTOCOL_HTTPS + host)
+            hosts.add(URL_RROTOCOL_HTTP + host)
+        }
+
+        return hosts
+    }
+
     companion object {
         private var authenticatedUser = MutableLiveData<User?>(null)
+
+        private val URL_RROTOCOL_HTTP = "http://"
+        private val URL_RROTOCOL_HTTPS = "https://"
     }
 }
