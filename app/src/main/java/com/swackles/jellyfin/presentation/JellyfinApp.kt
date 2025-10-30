0ba902319e15d82b3af5e3953dbdc473d17c070d
Swackles <swackles@users.noreply.github.com>
2025-10-30 20:20:55 +0200

    chore: changed com.swackles.jellyfin.presentation -> com.swackles.jellyfin.presentation.screens

--- app/src/main/java/com/swackles/jellyfin/presentation/JellyfinApp.kt
+++ app/src/main/java/com/swackles/jellyfin/presentation/JellyfinApp.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.swackles.jellyfin.presentation.screens.NavGraphs
 
 @Composable
 fun JellyfinApp() {
