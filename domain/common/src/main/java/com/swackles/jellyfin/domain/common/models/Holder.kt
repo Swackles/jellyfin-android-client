fc5edfdd85195312e7a6feb90ab5d93f5d0a7e90
Swackles <swackles@users.noreply.github.com>
2025-11-07 01:42:58 +0200

    chore: emptied out data and domain folders

--- domain/common/src/main/java/com/swackles/jellyfin/domain/common/models/Holder.kt
+++
@@ -1,7 +0,0 @@
-package com.swackles.jellyfin.domain.common.models
-
-sealed class Holder<T> {
-    class Success<T>(val data: T) : Holder<T>()
-    class Error<T>(val message: String) : Holder<T>()
-    class Loading<T> : Holder<T>()
-}
